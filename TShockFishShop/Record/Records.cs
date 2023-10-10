cb7860eaffe2927284f8601ae83aeb93a902bd05
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-10-10 15:22:43 +0530

    Update Records.cs

--- TShockFishShop/Record/Records.cs
+++ TShockFishShop/Record/Records.cs
@@ -1,222 +1,214 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using TShockAPI;
 
-namespace FishShop.Record;
-
-/// <summary>
-/// 购买记录
-/// </summary>
-public class Records
+namespace FishShop.Record
 {
-    static int lastSave = 0;
-    static bool isLoaded;
-    static RecordConfig _config;
-
-    public static string RecodFile = "";
-
     /// <summary>
-    /// 加载
+    /// Purchase records
     /// </summary>
-    public static void Load(bool forceLoad = false)
+    public class Records
     {
-        if (!isLoaded || forceLoad)
+        static int lastSave = 0;
+        static bool isLoaded;
+        static RecordConfig _config;
+
+        public static string RecodFile = "";
+
+        /// <summary>
+        /// Load records
+        /// </summary>
+        public static void Load(bool forceLoad = false)
         {
-            _config = RecordConfig.Load(RecodFile);
-            isLoaded = true;
-        }
-    }
-
-    /// <summary>
-    /// 保存
-    /// </summary>
-    private static void Save()
-    {
-        if (!isLoaded) return;
-
-        // 保存间隔在2s以上
-        if (utils.GetUnixTimestamp - lastSave > 2)
-        {
-            lastSave = utils.GetUnixTimestamp;
-            File.WriteAllText(RecodFile, JsonConvert.SerializeObject(_config, Formatting.Indented));
-        }
-    }
-
-    /// <summary>
-    /// 记录购买情况
-    /// </summary>
-    public static void Record(TSPlayer op, ShopItemData shopItemData, int amount, int costMoney, int costFish)
-    {
-        // 加载
-        Load();
-
-        int index = -1;
-        for (int i = 0; i < _config.player.Count; i++)
-        {
-            if (_config.player[i].name == op.Name)
+            if (!isLoaded || forceLoad)
             {
-                index = i;
-                break;
+                _config = RecordConfig.Load(RecodFile);
+                isLoaded = true;
             }
         }
 
-        if (index != -1)
+        /// <summary>
+        /// Save records
+        /// </summary>
+        private static void Save()
         {
-            _config.player[index].costMoney += costMoney;
-            _config.player[index].costFish += costFish;
-            List<RecordData> datas = _config.player[index].datas;
+            if (!isLoaded) return;
 
-            index = -1;
-            for (int i = 0; i < datas.Count; i++)
+            // Save if more than 2 seconds have passed since the last save
+            if (utils.GetUnixTimestamp - lastSave > 2)
             {
-                if (datas[i].id == shopItemData.id)
+                lastSave = utils.GetUnixTimestamp;
+                File.WriteAllText(RecodFile, JsonConvert.SerializeObject(_config, Formatting.Indented));
+            }
+        }
+
+        /// <summary>
+        /// Record a purchase
+        /// </summary>
+        public static void Record(TSPlayer op, ShopItemData shopItemData, int amount, int costMoney, int costFish)
+        {
+            // Load records
+            Load();
+
+            int index = -1;
+            for (int i = 0; i < _config.player.Count; i++)
+            {
+                if (_config.player[i].name == op.Name)
                 {
                     index = i;
                     break;
                 }
             }
 
-
             if (index != -1)
             {
-                datas[index].Record(amount);
+                _config.player[index].costMoney += costMoney;
+                _config.player[index].costFish += costFish;
+                List<RecordData> datas = _config.player[index].datas;
+
+                index = -1;
+                for (int i = 0; i < datas.Count; i++)
+                {
+                    if (datas[i].id == shopItemData.id)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+
+                if (index != -1)
+                {
+                    datas[index].Record(amount);
+                }
+                else
+                {
+                    datas.Add(new RecordData(shopItemData.name, shopItemData.id, amount));
+                }
             }
             else
             {
-                datas.Add(new RecordData(shopItemData.name, shopItemData.id, amount));
+                RecordPlayerData pd = new(op.Name);
+                pd.costMoney += costMoney;
+                pd.costFish += costFish;
+                pd.datas.Add(new RecordData(shopItemData.name, shopItemData.id, amount));
+                _config.player.Add(pd);
             }
 
-        }
-        else
-        {
-            RecordPlayerData pd = new(op.Name);
-            pd.costMoney += costMoney;
-            pd.costFish += costFish;
-            pd.datas.Add(new RecordData(shopItemData.name, shopItemData.id, amount));
-            _config.player.Add(pd);
+            // Save records
+            Save();
         }
 
-        // 保存
-        Save();
-    }
-
-    /// <summary>
-    /// 获得某个玩家的购买某件商品的次数
-    /// </summary>
-    /// <param name="op"></param>
-    /// <param name="goodsID"></param>
-    /// <returns></returns>
-    public static int GetPlayerRecord(TSPlayer op, int goodsID)
-    {
-        Load();
-        foreach (RecordPlayerData pd in _config.player)
+        /// <summary>
+        /// Get the number of times a player purchased a specific item
+        /// </summary>
+        public static int GetPlayerRecord(TSPlayer op, int goodsID)
         {
-            if (pd.name == op.Name)
+            Load();
+            foreach (RecordPlayerData pd in _config.player)
+            {
+                if (pd.name == op.Name)
+                {
+                    foreach (RecordData d in pd.datas)
+                    {
+                        if (d.id == goodsID)
+                            return d.count;
+                    }
+                    break;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Calculate the total sales quantity of a single item
+        /// </summary>
+        public static int CountShopItemRecord(int goodsID)
+        {
+            Load();
+
+            int count = -1;
+            foreach (RecordPlayerData pd in _config.player)
             {
                 foreach (RecordData d in pd.datas)
                 {
-                    if (d.id == goodsID)
-                        return d.count;
+                    if (d.id == goodsID) count += d.count;
                 }
-                break;
             }
+            return count;
         }
-        return -1;
-    }
 
-    /// <summary>
-    /// 计算单件商品的售出数量
-    /// </summary>
-    /// <param name="goodsID"></param>
-    /// <returns></returns>
-    public static int CountShopItemRecord(int goodsID)
-    {
-        Load();
-
-        int count = -1;
-        foreach (RecordPlayerData pd in _config.player)
+        /// <summary>
+        /// Reset records
+        /// </summary>
+        public static void ResetRecord()
         {
-            foreach (RecordData d in pd.datas)
+            Load();
+            _config.player.Clear();
+            Save();
+        }
+
+        /// <summary>
+        /// Display consumption rankings
+        /// </summary>
+        public static void ShowRank(CommandArgs args)
+        {
+            Load();
+            var lists = _config.player.Where(obj => obj.costMoney > 0).OrderByDescending(obj => obj.costMoney).ToList();
+            TSPlayer op = args.Player;
+
+            if (lists.Count == 0)
             {
-                if (d.id == goodsID) count += d.count;
+                op.SendInfoMessage("No consumption rankings available");
+                return;
             }
-        }
-        return count;
-    }
 
-    /// <summary>
-    /// 重置记录
-    /// </summary>
-    public static void ResetRecord()
-    {
-        Load();
-        _config.player.Clear();
-        Save();
-    }
+            if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, op, out int pageNumber))
+                return;
 
-    /// <summary>
-    /// 显示消费排行
-    /// </summary>
-    /// <param name="op"></param>
-    public static void ShowRank(CommandArgs args)
-    {
-        Load();
-        var lists = _config.player.Where(obj => obj.costMoney > 0).OrderByDescending(obj => obj.costMoney).ToList();
-        TSPlayer op = args.Player;
+            List<string> lines = new();
+            for (int i = 0; i < lists.Count; i++)
+            {
+                lines.Add($"#{i + 1}, {lists[i].name}, {utils.GetMoneyDesc(lists[i].costMoney)}");
+            }
 
-        if (lists.Count == 0)
-        {
-            op.SendInfoMessage("暂无 消费榜 数据");
-            return;
+            PaginationTools.SendPage(op, pageNumber, lines, new PaginationTools.Settings
+            {
+                HeaderFormat = "[c/96FF0A:'Consumption Rankings' ({0}/{1}):]",
+                FooterFormat = $"Enter /fish rank {{0}} to view more".SFormat(Commands.Specifier)
+            });
         }
 
-        if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, op, out int pageNumber))
-            return;
-
-        List<string> lines = new();
-        for (int i = 0; i < lists.Count; i++)
+        /// <summary>
+        /// Show fish basket rankings
+        /// </summary>
+        public static void ShowBasket(CommandArgs args)
         {
-            lines.Add($"第{i + 1}名, {lists[i].name}, {utils.GetMoneyDesc(lists[i].costMoney)}");
+            Load();
+            var lists = _config.player.Where(obj => obj.costFish > 0).OrderByDescending(obj => obj.costFish).ToList();
+            TSPlayer op = args.Player;
+
+            if (lists.Count == 0)
+            {
+                op.SendInfoMessage("No fish basket rankings available");
+                return;
+            }
+
+            if (!PaginationTools.TryParsePageNumber(args.Parameters, 2, op, out int pageNumber))
+                return;
+
+            List<string> lines = new();
+            for (int i = 0; i < lists.Count; i++)
+            {
+                lines.Add($"#{i + 1}, {lists[i].name}, {lists[i].costFish} fish");
+            }
+
+            PaginationTools.SendPage(op, pageNumber, lines, new PaginationTools.Settings
+            {
+                HeaderFormat = "[c/96FF0A:'Fish Basket Rankings' ({0}/{1}):]",
+                FooterFormat = $"Enter /fish basket {{0}} to view more".SFormat(Commands.Specifier)
+            });
         }
-
-        PaginationTools.SendPage(op, pageNumber, lines, new PaginationTools.Settings
-        {
-            HeaderFormat = "[c/96FF0A:『消费榜』({0}/{1})：]",
-            FooterFormat = "输入 /fish rank {{0}} 查看更多".SFormat(Commands.Specifier)
-        });
-    }
-
-    /// <summary>
-    /// 鱼篓
-    /// </summary>
-    public static void ShowBasket(CommandArgs args)
-    {
-        Load();
-        var lists = _config.player.Where(obj => obj.costFish > 0).OrderByDescending(obj => obj.costFish).ToList();
-        TSPlayer op = args.Player;
-
-        if (lists.Count == 0)
-        {
-            op.SendInfoMessage("暂无 鱼篓榜 数据");
-            return;
-        }
-
-
-        if (!PaginationTools.TryParsePageNumber(args.Parameters, 2, op, out int pageNumber))
-            return;
-
-        List<string> lines = new();
-        for (int i = 0; i < lists.Count; i++)
-        {
-            lines.Add($"第{i + 1}名, {lists[i].name}, {lists[i].costFish}条");
-        }
-
-        PaginationTools.SendPage(op, pageNumber, lines, new PaginationTools.Settings
-        {
-            HeaderFormat = "[c/96FF0A:『鱼篓榜』({0}/{1})：]",
-            FooterFormat = "输入 /fish basket {{0}} 查看更多".SFormat(Commands.Specifier)
-        });
     }
 }
