55a2d28d4c00384a3ebc234eab2a0d30d302445d
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-10-10 14:51:11 +0530

    Update Prefix.cs

--- TShockFishShop/Helper/Prefix.cs
+++ TShockFishShop/Helper/Prefix.cs
@@ -1,146 +1,145 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 
-namespace FishShop.Helper;
-
-public class Prefix
+namespace FishShop.Helper
 {
-    static readonly Dictionary<int, string> _prefixs = new()
+    public class Prefix
     {
-        {1, "大"},
-        {2, "巨大"},
-        {3, "危险"},
-        {4, "凶残"},
-        {5, "锋利"},
-        {6, "尖锐"},
-        {7, "微小"},
-        {8, "可怕"},
-        {9, "小"},
-        {10, "钝"},
-        {11, "倒霉"},
-        {12, "笨重"},
-        {13, "可耻"},
-        {14, "重"},
-        {15, "轻"}, //莱特
-        {16, "精准"},
-        {17, "迅速"},
-        {18, "急速"},
-        {19, "恐怖"},
-        {20, "致命"},
-        {21, "可靠"},
-        {22, "可畏"},
-        {23, "无力"},
-        {24, "粗笨"},
-        {25, "强大"},
-        {26, "神秘"},
-        {27, "精巧"},
-        {28, "精湛"},
-        {29, "笨拙"},
-        {30, "无知"},
-        {31, "错乱"},
-        {32, "威猛"},
-        {33, "禁忌"},
-        {34, "天界"},
-        {35, "狂怒"},
-        {36, "锐利"},
-        {37, "高端"},
-        {38, "强力"},
-        {39, "碎裂"},
-        {40, "破损"},
-        {41, "粗劣"},
-        {42, "迅捷"},
-        {43, "致命2"},
-        {44, "灵活"},
-        {45, "灵巧"},
-        {46, "残暴"},
-        {47, "缓慢"},
-        {48, "迟钝"},
-        {49, "呆滞"},
-        {50, "惹恼"},
-        {51, "凶险"},
-        {52, "狂躁"},
-        {53, "致伤"},
-        {54, "强劲"},
-        {55, "粗鲁"},
-        {56, "虚弱"}, // 软弱
-        {57, "无情"},
-        {58, "暴怒"},
-        {59, "神级"},
-        {60, "恶魔"},
-        {61, "狂热"},
-        {62, "坚硬"},
-        {63, "守护"},
-        {64, "装甲"},
-        {65, "护佑"},
-        {66, "奥秘"},
-        {67, "精确"},
-        {68, "幸运"},
-        {69, "锯齿"},
-        {70, "尖刺"},
-        {71, "愤怒"},
-        {72, "险恶"},
-        {73, "轻快"},
-        {74, "快速"},
-        {75, "急速2"},
-        {76, "迅捷2"},
-        {77, "狂野"},
-        {78, "鲁莽"},
-        {79, "勇猛"},
-        {80, "暴力"},
-        {81, "传奇"},
-        {82, "虚幻"},
-        {83, "神话"},
-        {84, "传奇2"}, // 泰拉悠悠球
-    };
-
-
-    public static int GetPrefix(string idOrName)
-    {
-        if (int.TryParse(idOrName, out int num))
+        static readonly Dictionary<int, string> _prefixes = new()
         {
-            if (num <= 84 && num > 0)
+            {1, "Large"},
+            {2, "Huge"},
+            {3, "Dangerous"},
+            {4, "Ferocious"},
+            {5, "Sharp"},
+            {6, "Pointed"},
+            {7, "Tiny"},
+            {8, "Terrifying"},
+            {9, "Small"},
+            {10, "Dull"},
+            {11, "Unlucky"},
+            {12, "Clumsy"},
+            {13, "Shameful"},
+            {14, "Heavy"},
+            {15, "Light"},
+            {16, "Precise"},
+            {17, "Swift"},
+            {18, "Rapid"},
+            {19, "Terrifying"},
+            {20, "Deadly"},
+            {21, "Reliable"},
+            {22, "Dreadful"},
+            {23, "Feeble"},
+            {24, "Clunky"},
+            {25, "Powerful"},
+            {26, "Mysterious"},
+            {27, "Exquisite"},
+            {28, "Masterful"},
+            {29, "Awkward"},
+            {30, "Ignorant"},
+            {31, "Chaotic"},
+            {32, "Mighty"},
+            {33, "Forbidden"},
+            {34, "Celestial"},
+            {35, "Furious"},
+            {36, "Sharp"},
+            {37, "High-End"},
+            {38, "Strong"},
+            {39, "Fractured"},
+            {40, "Broken"},
+            {41, "Rough"},
+            {42, "Swift"},
+            {43, "Deadly II"},
+            {44, "Agile"},
+            {45, "Nimble"},
+            {46, "Savage"},
+            {47, "Slow"},
+            {48, "Dull"},
+            {49, "Stupid"},
+            {50, "Annoying"},
+            {51, "Dangerous"},
+            {52, "Manic"},
+            {53, "Wounding"},
+            {54, "Potent"},
+            {55, "Rough"},
+            {56, "Weak"},
+            {57, "Merciless"},
+            {58, "Furious"},
+            {59, "Godly"},
+            {60, "Demonic"},
+            {61, "Fanatical"},
+            {62, "Hard"},
+            {63, "Guarding"},
+            {64, "Armored"},
+            {65, "Warding"},
+            {66, "Mystic"},
+            {67, "Accurate"},
+            {68, "Lucky"},
+            {69, "Serrated"},
+            {70, "Spiked"},
+            {71, "Angry"},
+            {72, "Wicked"},
+            {73, "Nimble"},
+            {74, "Quick"},
+            {75, "Rapid II"},
+            {76, "Swift II"},
+            {77, "Wild"},
+            {78, "Rash"},
+            {79, "Valiant"},
+            {80, "Violent"},
+            {81, "Legendary"},
+            {82, "Illusory"},
+            {83, "Mythical"},
+            {84, "Legendary II"},
+        };
+
+        public static int GetPrefix(string idOrName)
+        {
+            if (int.TryParse(idOrName, out int num))
             {
-                return num;
-            }else
-            {
-                return 0;
+                if (num <= 84 && num > 0)
+                {
+                    return num;
+                }
+                else
+                {
+                    return 0;
+                }
             }
-        }
-        switch (idOrName)
-        {
-            case "莱特": return 15;
-            case "软弱": return 56;
-        }
-        var li = _prefixs.Where(obj => obj.Value == idOrName);
-        if (li.Any())
-        {
-            return li.First().Key;
-        }
-        return 0;
-    }
-
-
-    public static string GetName(int prefix)
-    {
-        if (_prefixs.ContainsKey(prefix))
-        {
-            return _prefixs[prefix];
-        }
-        return "";
-    }
-
-
-    public static bool CanHavePrefixes(Item item)
-    {
-        if (item.type != 0 && item.maxStack == 1)
-        {
-            if (item.damage <= 0)
+            switch (idOrName)
             {
-                return item.accessory;
+                case "Light": return 15;
+                case "Weak": return 56;
             }
-            return true;
+            var li = _prefixes.Where(obj => obj.Value == idOrName);
+            if (li.Any())
+            {
+                return li.First().Key;
+            }
+            return 0;
         }
-        return false;
+
+        public static string GetName(int prefix)
+        {
+            if (_prefixes.ContainsKey(prefix))
+            {
+                return _prefixes[prefix];
+            }
+            return "";
+        }
+
+        public static bool CanHavePrefixes(Item item)
+        {
+            if (item.type != 0 && item.maxStack == 1)
+            {
+                if (item.damage <= 0)
+                {
+                    return item.accessory;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
