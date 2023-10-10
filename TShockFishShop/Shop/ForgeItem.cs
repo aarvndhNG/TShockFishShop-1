0287415430caf07a9fab558ce2bee4f16a210d0a
That Blackwolf <107058543+aarvndhNG@users.noreply.github.com>
2023-10-10 15:26:29 +0530

    Update ForgeItem.cs

--- TShockFishShop/Shop/ForgeItem.cs
+++ TShockFishShop/Shop/ForgeItem.cs
@@ -1,140 +1,136 @@
-﻿using FishShop.Helper;
+using FishShop.Helper;
 using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 
-namespace FishShop.Shop;
-
-public class ForgeItem : ShopItem
+namespace FishShop.Shop
 {
-    public ForgeItem(ShopItemData si) : base(si)
+    public class ForgeItem : ShopItem
     {
-    }
-
-    // 能否购买
-    public override string CanBuy()
-    {
-        var msg = base.CanBuy();
-        if (msg != "") return msg;
-
-        Item forgeItem = op.TPlayer.inventory[0];
-        int id = forgeItem.netID;
-        if (id == 0) return "需将要重铸的物品，放在背包的第1格！";
-        if (!Prefix.CanHavePrefixes(forgeItem)) return $"[i:{id}]不能重铸";
-
-        if (extra == "")
+        public ForgeItem(ShopItemData si) : base(si)
         {
-            return "需在购买指令后面加上词缀名称，例如：/fish buy 重铸 虚幻";
         }
-        else
+
+        public override string CanBuy()
         {
+            var msg = base.CanBuy();
+            if (msg != "") return msg;
+
+            Item forgeItem = op.TPlayer.inventory[0];
+            int id = forgeItem.netID;
+            if (id == 0) return "You need to place the item to be reforged in the first slot of your inventory!";
+            if (!Prefix.CanHavePrefixes(forgeItem)) return $"[i:{id}] cannot be reforged";
+
+            if (extra == "")
+            {
+                return "You need to specify a prefix name after the buy command, for example: /fish buy reforging Mythical";
+            }
+            else
+            {
+                byte targetPrefix = (byte)Prefix.GetPrefix(extra);
+                if (targetPrefix == forgeItem.prefix)
+                {
+                    return "The item already has this prefix, no need to reforge!";
+                }
+                else if (targetPrefix == 0)
+                {
+                    return "Invalid prefix input! You can use numbers 1-84 instead of Chinese names.";
+                }
+            }
+
+            var npc = NPCHelper.FindNearNPC(op, 107);
+            if (npc == null) return "You need to be near the Goblin Tinkerer!";
+
+            long ownedCoins = InventoryHelper.GetCoinsCount(op);
+            int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
+            if (ownedCoins < needCoins)
+            {
+                return $"Not enough money! The budget for this reforge is {utils.GetMoneyDesc(needCoins)}";
+            }
+
+            return "";
+        }
+
+        public override void DeductCost(out int costMoney, out int costFish)
+        {
+            var npc = NPCHelper.FindNearNPC(op, 107);
+            Item forgeItem = op.TPlayer.inventory[0];
+            int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
+            costMoney = needCoins;
+            costFish = 0;
+            InventoryHelper.DeductMoney(op, needCoins);
+        }
+
+        public override void ProvideGoods()
+        {
+            Item forgeItem = op.TPlayer.inventory[0];
+            int id = forgeItem.netID;
+
+            Item item = new Item();
+            item.SetDefaults(id);
+            item.prefix = forgeItem.prefix;
+
+            var npc = NPCHelper.FindNearNPC(op, 107);
+
+            List<byte> history = new List<byte>();
+            long totalCoins = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                totalCoins += ForgeCost(item, op.TPlayer, npc);
+                item.ResetPrefix();
+                item.Prefix(-2);
+                history.Add(item.prefix);
+            }
+
             byte targetPrefix = (byte)Prefix.GetPrefix(extra);
-            if (targetPrefix == forgeItem.prefix)
+            var last = targetPrefix;
+
+            int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
+            var coinsTips = $"Cost: {utils.GetMoneyDesc(needCoins)} | Balance: {InventoryHelper.GetCoinsCountDesc(op)}";
+            var li = history.Select(p => Prefix.GetName(p));
+            var hisStr = " | " + string.Join(" ", li);
+            if (history.Contains(targetPrefix))
             {
-                return "现在的词缀就是，不需要再重铸了！";
+                var luckyNum = history.IndexOf(targetPrefix) + 1;
+                op.SendSuccessMessage($"Reforge Successful: [i/p{targetPrefix}:{id}] | {coinsTips}{hisStr}");
             }
-            else if (targetPrefix == 0)
+            else
             {
-                return "词缀输入错误！可用数字 1~84 代替中文名称。";
+                last = history.Last();
+                var tips = "";
+                if (!byte.TryParse(extra, out byte tempPrefix))
+                {
+                    tips = $" | To reforge again, you can input /fish forge {targetPrefix}";
+                }
+                op.SendInfoMessage($"Reforge Completed: [i/p{last}:{id}] | {coinsTips}{tips}{hisStr}");
             }
-        }
 
-        var npc = NPCHelper.FindNearNPC(op, 107);
-        if (npc == null) return "需要在哥布林工匠附近！";
+            forgeItem.prefix = last;
+            utils.PlayerSlot(op, forgeItem, 0);
 
-        long ownedCoins = InventoryHelper.GetCoinsCount(op);
-        int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
-        if (ownedCoins < needCoins)
-        {
-            return $"钱不够！本次重铸的预算是{utils.GetMoneyDesc(needCoins)}";
-        }
+            op.SendData(PacketTypes.PlayerSlot, "", op.Index, 0, last);
 
-        return "";
-    }
-
-    public override void DeductCost(out int costMoney, out int costFish)
-    {
-        var npc = NPCHelper.FindNearNPC(op, 107);
-        Item forgeItem = op.TPlayer.inventory[0];
-        int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
-        costMoney = needCoins;
-        costFish = 0;
-        InventoryHelper.DeductMoney(op, needCoins);
-    }
-
-    public override void ProvideGoods()
-    {
-        Item forgeItem = op.TPlayer.inventory[0];
-        int id = forgeItem.netID;
-
-        Item item = new();
-        item.SetDefaults(id);
-        item.prefix = forgeItem.prefix;
-
-        var npc = NPCHelper.FindNearNPC(op, 107);
-
-        List<byte> history = new();
-        long totalCoins = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            totalCoins += ForgeCost(item, op.TPlayer, npc);
-            item.ResetPrefix();
-            item.Prefix(-2);
-            history.Add(item.prefix);
-        }
-
-        byte targetPrefix = (byte)Prefix.GetPrefix(extra);
-        var last = targetPrefix;
-
-        int needCoins = ForgeCost(forgeItem, op.TPlayer, npc) * 10;
-        var coinsTips = $"花费: {utils.GetMoneyDesc(needCoins)} | 余额: {InventoryHelper.GetCoinsCountDesc(op)}";
-        var li = history.Select(p => Prefix.GetName(p));
-        var hisStr = " | " + string.Join(" ", li);
-        if (history.Contains(targetPrefix))
-        {
-            var luckyNum = history.IndexOf(targetPrefix) + 1;
-            op.SendSuccessMessage($"重铸成功: [i/p{targetPrefix}:{id}] | {coinsTips}{hisStr}");
-        }
-        else
-        {
-            last = history.Last();
-            var tips = "";
-            if (!byte.TryParse(extra, out byte tempPrefix))
+            var remain = needCoins - totalCoins;
+            if (remain > 0)
             {
-                tips = $" | 再次重铸可输入 /fish forge {targetPrefix}";
+                InventoryHelper.Refund(op, remain);
+                op.SendInfoMessage($"Budget surplus, refunding {utils.GetMoneyDesc(remain)}");
             }
-            op.SendInfoMessage($"重铸完成: [i/p{last}:{id}] | {coinsTips}{tips}{hisStr}");
+            op.SendInfoMessage($"{needCoins}   {totalCoins}");
         }
 
-
-        // 更新物品词缀
-        //forgeItem.Prefix(last);
-        forgeItem.prefix = last;
-        utils.PlayerSlot(op, forgeItem, 0);
-
-        op.SendData(PacketTypes.PlayerSlot, "", op.Index, 0, last);
-
-        // 归还
-        var remain = needCoins - totalCoins;
-        if (remain > 0)
+        static int ForgeCost(Item item, Player plr, NPC npc)
         {
-            InventoryHelper.Refund(op, remain);
-            op.SendInfoMessage($"预算结余，返还 {utils.GetMoneyDesc(remain)}");
-        }
-        op.SendInfoMessage($"{needCoins}   {totalCoins}");
-    }
+            int coins = item.value;
+            if (plr.discountAvailable)
+            {
+                coins = (int)(coins * 0.8);
+            }
+            var settings = Main.ShopHelper.GetShoppingSettings(plr, npc);
+            coins = (int)(coins * settings.PriceAdjustment);
+            coins /= 3;
 
-    static int ForgeCost(Item item, Player plr, NPC npc)
-    {
-        int coins = item.value;
-        if (plr.discountAvailable)
-        {
-            coins = (int)(coins * 0.8);
+            return coins;
         }
-        var settings = Main.ShopHelper.GetShoppingSettings(plr, npc);
-        coins = (int)(coins * settings.PriceAdjustment);
-        coins /= 3;
-
-        return coins;
     }
 }
